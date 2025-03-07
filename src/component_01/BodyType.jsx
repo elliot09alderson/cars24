f7fd839d98bfcdc0c5fac0bdd0719ad74bad05cf
ui-yashaswi <itsgeekyyashaswi@gmail.com>
2025-03-07 20:30:03 +0530

    cars 24 cards

---
+++ src/component_01/BodyType.jsx
@@ -0,0 +1,62 @@
+import { ChevronDown } from "lucide-react";
+import React, { useState } from "react";
+
+const BodyType = () => {
+  const [open, setOpen] = useState(false);
+
+  const BodyTypeData = [
+    {
+      bodyName: "SUV",
+      count: 15,
+      img: "	https://media.cars24.com/india/buy/facets_v4/body_type/22072024/SUV.png",
+      checked: false,
+    },
+    {
+      bodyName: "Sedan",
+      count: 15,
+      img: "	https://media.cars24.com/india/buy/facets_v4/body_type/22072024/Sedan.png",
+      checked: false,
+    },
+    {
+      bodyName: "Hatchback",
+      count: 15,
+      img: "	https://media.cars24.com/india/buy/facets_v4/body_type/22072024/Hatchback.png",
+      checked: false,
+    },
+  ];
+
+  return (
+    <div className="flex flex-col ">
+      <div
+        className="flex justify-between pb-6 cursor-pointer"
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        <p className="text-lg font-semibold">Body Type</p>
+        <ChevronDown
+          className={`bg-gray-100 rounded-full p-1 transition-transform duration-300 ${
+            open ? "rotate-180" : ""
+          }`}
+        />
+      </div>
+      {open ? (
+        <div className="flex flex-wrap gap-2 items-center  ">
+       {BodyTypeData.map((item,idx)=>(
+        <div className="w-30 items-center p-8 bg-gray-200 rounded-lg ">
+            <div className="flex gap-2 items-center flex-col">
+
+            <img className="w-16 object-cover h-6" src={item.img} alt="" />
+            <p className="text-sm font-semibold ">{item.bodyName}</p>
+            <p className="text-sm text-gray-400">({item.count})</p>
+            </div>
+        </div>
+
+       ))}
+        </div>
+      ) : (
+        <div className="border-b border border-gray-200"></div>
+      )}
+    </div>
+  );
+};
+
+export default BodyType;
---
+++ src/component_01/ModelYear.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+const ModelYear = () => {
+  const [range, setRange] = useState(2010);
+  const [kms, setKms] = useState(1000);
+
+  return (
+    <div>
+      <div>
+        <h1 className="text-lg py-4 font-semibold">Model Year</h1>
+
+        <div className="pl-2 flex gap-2">
+          <span>2010</span>
+
+          <input
+            type="range"
+            className="w-full "
+            min={2010}
+            max={2025}
+            value={range}
+            onChange={(e) => {
+              setRange(e.target.value);
+            }}
+          />
+          <span>2025</span>
+        </div>
+        <h2 className="flex items-center justify-center text-sm font-semibold">
+          {range}
+        </h2>
+      </div>
+      <div>
+        <h1 className="text-lg py-4 font-semibold">Kms Driven</h1>
+
+        <div className="pl-2 flex gap-2">
+          <span>1000</span>
+
+          <input
+            type="range"
+            className="w-full "
+            min={1000}
+            max={1250000}
+            value={kms}
+            onChange={(e) => {
+              setKms(e.target.value);
+            }}
+          />
+          <span>1250000</span>
+        </div>
+        <h2 className="flex items-center justify-center text-sm font-semibold">
+          {kms}
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default ModelYear;
